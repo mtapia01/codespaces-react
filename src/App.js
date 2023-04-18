f60cb5e9561852357f19e6599b650bc202f6a0f5
Michael Tapia <73376610+mtapia01@users.noreply.github.com>
2023-04-18 04:53:09 +0000

    feat(App.css, App.js): Added total savings from grid

--- src/App.js
+++ src/App.js
@@ -5,60 +5,72 @@ import React, { useState } from 'react';
 
 function App() {
   let [billSolar, setSolar] = React.useState(0)
-  let [totalBill, setTotalBill] = React.useState(165)
+  let [totalBillSolar, setTotalBillSolar] = React.useState(0)
 
   const handleSolar = (event) => {
     let solarCredit = parseFloat(event.target.value);
 
     setSolar(solarCredit * .08);
-    setTotalBill(150 + 15 - solarCredit)
+    setTotalBillSolar(150 + 15 - (solarCredit * .08))
   }
 
-  
-
-  let [bill, setBill] = React.useState(0)
+  let [billGrid, setBill] = React.useState(0);
   let handleGrid = (event) => {
     let kwhUsed = parseFloat(event.target.value);
 
     setBill(kwhUsed * .37)
-    // console.log(setBill(kwhUsed * .37))
   };
-
 
   return (
     <main>
       <div className="App">
         <header className="App-header">
           <h>Policy Analyzer</h>
+
           <div>
             <input
               type="number"
               onChange={handleGrid}
               placeholder='kWh Used (month)'>
             </input>
+
             <input
               type='number'
               let onChange={handleSolar}
               placeholder='# of Hrs Away (month)'>
             </input>
-            
           </div>
+
+          <br />
+
           <div>
             <u>Grid</u>
             {/* using San Diego kwh price */}
-            <p>Bill amount: ${bill.toFixed(2)} </p>
+            <p>Bill amount: ${billGrid.toFixed(2)} </p>
           </div>
-          <div>
-            <u>Solar (under NEM 3.0)</u>
-            <p>Solar Credit: ${billSolar.toFixed(2)} </p> <br />
-            {/* average loan is between 50-250 so will say 150 */}
-            Loan Payment: ~$150 <br />
-            Monthly fee: $15 <br />
 
-            Total Bill (Estimate): ${totalBill.toFixed(2)}
-          </div>
           <div>
+          <p><u>Solar (under NEM 3.0)</u></p>
+
+            <p3 >
+              Solar Credit: ${billSolar.toFixed(2)}
+            </p3> <br />
+
+            <p3 >
+            {/* average loan is between 50-250 so we will say 150 */}
+              Loan Payment: ~$150 <br />
+              Monthly fee: $15 <br />
+            </p3>
+            Total Bill (Estimate): ${totalBillSolar.toFixed(2)}
           </div>
+
+          <br />
+
+
+          <div>
+            Switching to solar you will save ${(billGrid - totalBillSolar).toFixed(2)}
+          </div>
+
         </header>
       </div>
     </main>
