32efeff2111771830e0dd22eeb456f6070ee549b
Michael Tapia <73376610+mtapia01@users.noreply.github.com>
2023-04-18 04:20:20 +0000

    feat(App.js): Working basic calculator

--- src/App.js
+++ src/App.js
@@ -1,66 +1,63 @@
 import './App.css';
 import React, { useState } from 'react';
 
+
+
 function App() {
-  const [message, setMessage] = useState('');
+  let [billSolar, setSolar] = React.useState(0)
+  let [totalBill, setTotalBill] = React.useState(165)
 
-  const [updated, setUpdated] = useState(message);
+  const handleSolar = (event) => {
+    let solarCredit = parseFloat(event.target.value);
 
-  const handleChange = (event) => {
-    setMessage();
-  };
-
-  const handleClick = () => {
-    // 👇 "message" stores input field value
-    setUpdated("CurrentBill");
-  };
+    setSolar(solarCredit * .08);
+    setTotalBill(150 + 15 - solarCredit)
+  }
 
   
+
+  let [bill, setBill] = React.useState(0)
+  let handleGrid = (event) => {
+    let kwhUsed = parseFloat(event.target.value);
+
+    setBill(kwhUsed * .37)
+    // console.log(setBill(kwhUsed * .37))
+  };
+
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     {
-    //       <h>Policy Analyzer</h>
-    //     }
-    //     <div>
-    //       <body>
-    //         <input 
-    //           type="number" 
-    //           placeholder='Current Bill Amount'
-    //           id='CurrentBill'
-    //           onChange={handleChange}
-    //           value={message}
-    //         />
-    //         <button 
-    //           id='currentBillBTN' 
-    //           onClick={handleClick}>
-    //           Submit
-    //         </button>
-    //       </body>
-    //     </div>
-    //   </header>
-    // </div>
     <main>
       <div className="App">
         <header className="App-header">
-          {
-            <h>Policy Analyzer</h>
-          }
+          <h>Policy Analyzer</h>
           <div>
-            <body>
-              <input
-                type="number"
-                placeholder='Current Bill Amount'
-                id='CurrentBill'
-                onChange={handleChange}
-                value={message}
-              />
-              <button
-                id='currentBillBTN'
-                onClick={handleClick}>
-                Submit
-              </button>
-            </body>
+            <input
+              type="number"
+              onChange={handleGrid}
+              placeholder='kWh Used (month)'>
+            </input>
+            <input
+              type='number'
+              let onChange={handleSolar}
+              placeholder='# of Hrs Away (month)'>
+            </input>
+            
+          </div>
+          <div>
+            <u>Grid</u>
+            {/* using San Diego kwh price */}
+            <p>Bill amount: ${bill.toFixed(2)} </p>
+          </div>
+          <div>
+            <u>Solar (under NEM 3.0)</u>
+            <p>Solar Credit: ${billSolar.toFixed(2)} </p> <br />
+            {/* average loan is between 50-250 so will say 150 */}
+            Loan Payment: ~$150 <br />
+            Monthly fee: $15 <br />
+
+            Total Bill (Estimate): ${totalBill.toFixed(2)}
+          </div>
+          <div>
           </div>
         </header>
       </div>
